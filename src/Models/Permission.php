2dccbe98c2dbcb3146b2e4e01b57f2434aa5782a
Mikaël Popowicz <mikael.popowicz@gmail.com>
2019-11-20 18:59:18 +0100

    Store canOverridePermission in cache.

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -5,6 +5,8 @@ namespace BBSLab\NovaPermission\Models;
 use BBSLab\NovaPermission\Contracts\Permission as PermissionContract;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Spatie\Permission\Models\Permission as Model;
 use Spatie\Permission\PermissionRegistrar;
 
@@ -17,12 +19,26 @@ use Spatie\Permission\PermissionRegistrar;
  * @property string $group
  * @property int $authorizable_id
  * @property string $authorizable_type
+ * @property \Illuminate\Database\Eloquent\Model $authorizable
  * @property \Illuminate\Database\Eloquent\Collection $roles
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  */
 class Permission extends Model implements PermissionContract
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function (Permission $permission) {
+            $permission->forgetPermissionFromCache();
+        });
+
+        static::deleting(function (Permission $permission) {
+            $permission->forgetPermissionFromCache();
+        });
+    }
+
     /**
      * Get the authorizable instance.
      *
@@ -61,5 +77,20 @@ class Permission extends Model implements PermissionContract
                 })->toArray()
             ),
         ];
+    }
+
+    public function forgetPermissionFromCache()
+    {
+        if ($this->authorizable) {
+            $key = implode(':', [
+                'nova-permission',
+                'authorization',
+                $this->getOriginal('authorizable_type', $this->authorizable_type),
+                $this->getOriginal('authorizable_id', $this->authorizable_id),
+                Str::snake($this->getOriginal('name', $this->name))
+            ]);
+
+            Cache::forget($key);
+        }
     }
 }
--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -3,37 +3,76 @@
 namespace BBSLab\NovaPermission\Traits;
 
 use BBSLab\NovaPermission\Contracts\HasAuthorizations;
+use Illuminate\Support\Str;
+use Spatie\Permission\PermissionRegistrar;
 use Spatie\Permission\Traits\HasRoles as BaseTrait;
 
 trait HasRoles
 {
     use BaseTrait;
 
+    public function getOverridePermissionCacheTags(): array
+    {
+        return ['nova-permission', 'can-override'];
+    }
+
+    public function getOverridePermissionCacheKey(): string
+    {
+        return implode(':', [
+            'nova-permission',
+            'can-override',
+            'user:'.$this->getKey(),
+        ]);
+    }
+
     public function canOverridePermission(): bool
     {
-        $guard = config('nova.guard') ?? config('auth.defaults.guard');
+        /** @var \Illuminate\Contracts\Cache\Factory $cacheManager */
+        $cacheManager = app('cache');
 
-        return $this->roles()
-            ->where('guard_name', '=', $guard)
-            ->where('override_permission', '=', true)
-            ->exists();
+        $cache = method_exists($cacheManager->store(), 'tags')
+            ? $cacheManager->store()->tags($this->getOverridePermissionCacheTags())
+            : $cacheManager->store();
+
+        $key = $this->getOverridePermissionCacheKey();
+
+        return $cache->remember($key, PermissionRegistrar::$cacheExpirationTime, function () {
+            $guard = config('nova.guard') ?? config('auth.defaults.guard');
+
+            return $this->roles()
+                ->where('guard_name', '=', $guard)
+                ->where('override_permission', '=', true)
+                ->exists();
+        });
     }
 
     public function hasPermissionToOnModel($permission, $model = null, $guardName = null): bool
     {
         if (empty($model) || ! $model instanceof HasAuthorizations) {
-            return $this->hasPermissionTo($permission, $guardName);
+            return $this->can($permission);
         }
 
-        $authorization = $model->authorizations()
-            ->where('name', '=', $permission)
-            ->when(! empty($guardName), function ($query) use ($guardName) {
-                $query->where('guard_name', '=', $guardName);
-            })
-            ->first();
+        $key = implode(':', [
+            'nova-permission',
+            'authorization',
+            class_basename($model),
+            $model->getKey(),
+            Str::snake($permission)
+        ]);
+
+        /** @var \Illuminate\Contracts\Cache\Factory $cacheManager */
+        $cacheManager = app('cache');
+
+        $authorization = $cacheManager->store()
+            ->remember($key, PermissionRegistrar::$cacheExpirationTime, function () use ($permission, $model, $guardName) {
+                return $model->authorizations()
+                    ->where('name', '=', $permission)
+                    ->where('guand_name', '=', $guardName ?? $this->getDefaultGuardName())
+                    ->first();
+            });
 
         return ! empty($authorization)
             ? $this->hasPermissionTo($authorization)
-            : $this->hasPermissionTo($permission, $guardName);
+            : $this->can($permission);
     }
 }
